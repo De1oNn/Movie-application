9ab2e6aa04256bedb28c85d2a4ee681e48ccee90
Nasan Jargal <nasanjargal20040524@gmail.com>
2025-01-29 20:00:10 +0800

    commit

--- src/app/component/comment/Comment.tsx
+++ src/app/component/comment/Comment.tsx
@@ -1,10 +1,21 @@
+import { Textarea } from "@/components/ui/textarea"
+import { Plus } from 'lucide-react';
+
 export const Comment = () => {
     return (
-        <div className="min-h-[250px] w-[100%]  p-[20px] ">
+        <div className="min-h-[150px] w-[100%]  p-[20px] ">
             <div className="h-[100%] w-[100%] rounded-[20px] border-[3px] p-[10px]">
-                <p className="text-[#064ba4] font-semibold">Comment</p>
-                <input className="w-[100%] h-[60px] border-[2px] rounded-[10px] p-[10px] mt-[20px]" placeholder="Comment"/>
+                <div>
+                    <p className="text-[#064ba4] font-semibold">Comment</p>
+                </div>
+                <div className="flex justify-between mt-[10px]">
+                    {/* <input className="w-[85%] h-[60px] border-[2px] rounded-[10px]  p-[10px]" placeholder="Comment"/> */}
+                    <Textarea className="w-[85%] h-[60px] border-[2px] rounded-[10px]  p-[10px]" placeholder="Comment"/>
+                    <div className="flex justify-center items-center h-[80px] w-[40px]">
+                        <button className="h-[50%] w-[100%] border-[4px] rounded-[10px] bg-[#0480c7] border-[#064ba4] text-[white] flex justify-center items-center"><Plus /></button>
+                    </div>
+                </div>
             </div>
         </div>
     )
-}
+}  
