f5c00544671ccec57e3864b2cafcabc2b5fddd87
De1oNn <nasanjargal20040524@gmail.com>
2025-02-03 12:41:15 +0800

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,16 @@ import { useRouter } from "next/navigation";
 const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
 const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN;
 
+type TvShow = {
+  id: number;
+  name: string;
+  overview: string;
+  backdrop_path: string;
+  poster_path: string;
+  vote_average: number;
+  vote_count: number;
+};
+
 const Page = () => {
   type Movie = {
     id: number;
@@ -21,11 +31,13 @@ const Page = () => {
     release_date: number;
   };
   
+  
 
   const [nowPlayingMovies, setNowPlayingMovies] = useState<Movie[]>([]); // State for Now Playing
   const [upcomingMovies, setUpcomingMovies] = useState<Movie[]>([]);
   const [popularMovies, setPopularMovies] = useState<Movie[]>([]);
-  const [topRatedMovies, setTopRatedMovies] = useState<Movie[]>([]);    // State for Upcoming
+  const [topRatedMovies, setTopRatedMovies] = useState<Movie[]>([]); 
+  const [Tv, setTv] = useState<TvShow[]>([]) 
   const [currentIndex, setCurrentIndex] = useState(0);
   const router = useRouter();
 
@@ -89,8 +101,22 @@ const Page = () => {
         }
       );
       setTopRatedMovies(response.data.results); 
-      console.log(response.data.results);
       
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  };
+  const getTv = async () => {
+    try {
+      const response = await axios.get(
+        `${TMDB_BASE_URL}/tv/top_rated?language=en-US&page=1`, 
+        {
+          headers: {
+            Authorization: `Bearer ${TMDB_API_TOKEN}`,
+          },
+        }
+      );
+      setTv(response.data.results); // Set the fetched TV shows to state
     } catch (err) {
       console.error("Error:", err);
     }
@@ -102,6 +128,7 @@ const Page = () => {
     getUpcomingMovieData(); 
     getPopularMovieData();
     getTopRatedMovieData();
+    getTv();
   }, []);
 
   const handleNextMovie = () => {
@@ -120,6 +147,7 @@ const Page = () => {
 
   return (
     <div className="flex flex-col items-center justify-center w-[100%]">
+
       {nowPlayingMovies.length > 0 && (
         <div
           className="flex flex-col items-start border-[2px] rounded p-4 cursor-pointer transition-transform hover:scale-[1.02] hover:shadow-md lg:hidden  "
@@ -219,45 +247,6 @@ const Page = () => {
           </div>
         </div>
       )}
-        
-      {/* {nowPlayingMovies.length > 0 && (
-        <div className="flex flex-col items-start border-[2px] rounded p-4 cursor-pointer transition-transform hover:scale-[1.02] hover:shadow-md">
-          {nowPlayingMovies[currentIndex].backdrop_path && (
-            <img
-              src={`https://image.tmdb.org/t/p/original${nowPlayingMovies[currentIndex].backdrop_path}`}
-              alt={nowPlayingMovies[currentIndex].title}
-              className="rounded-md h-[246px] w-full"
-            />
-          )}
-          <div className="p-[20px] min-h-[220px] max-h-[380px] w-full">
-            <div className="flex justify-between">
-              <div>
-                <p>Now Playing</p>
-                <h1 className="text-xl font-bold">{nowPlayingMovies[currentIndex].title}</h1>
-              </div>
-              <div className="flex">
-                <Star className="h-[20px] w-[20px] mr-[10px]" />
-                <div className="flex flex-col">
-                  {nowPlayingMovies[currentIndex].vote_average}/10
-                  <span className="flex justify-center items-center">
-                    <Users className="h-[20px] w-[20px] mr-[10px]" />
-                    {nowPlayingMovies[currentIndex].vote_count}
-                  </span>
-                </div>
-              </div>
-            </div>
-            <p className="text-[14px] min-h-[100px] w-[350px]">
-              {nowPlayingMovies[currentIndex].overview}
-            </p>
-            <div className="h-[36px] w-[144px] mt-[20px]">
-              <button className="py-[8px] px-[16px] flex justify-between h-[36px] w-[144px] bg-[black] text-[14px] text-[white] rounded-[10px] justify-center items-center">
-                <Play className="text-[white] h-[15px] w-[15px]" />
-                Watch Trailer
-              </button>
-            </div>
-          </div>
-        </div>
-      )} */}
 
       {/* Upcoming Movies Section */}
       {upcomingMovies.length > 0 && (
@@ -356,6 +345,42 @@ const Page = () => {
           </div>
         </div>
       )}
+
+        <div className=" mt-6 flex justify-center items-center flex-col">
+          <div className="flex justify-between w-[100%] h-[36px]">
+            <h1 className="font-semibold text-[24px] mb-4">Top Rated TV Shows</h1>
+            <button className="h-full w-[120px] border-b-2 border-transparent hover:border-black transition duration-300 cursor-pointer transition-transform hover:scale-[1.02]">See more</button>
+          </div>
+          <div className="grid grid-cols-2 gap-4 mt-4 lg:grid-cols-5 lg:gap-8 w-[90%] lg:w-[100%]">
+            {Tv.slice(0, 10).map((show) => (
+              <div
+                key={show.id}
+                className="lg:w-[230px] flex flex-col items-center border rounded-md bg-[#c2c2d5] lg:h-[430px] cursor-pointer transition-transform hover:scale-[1.02] hover:shadow-md"
+              >
+                {show.poster_path && (
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${show.poster_path}`}
+                    alt={show.name}
+                    className="rounded-md w-[100%] h-[225px] lg:h-[340px]"
+                  />
+                )}
+                <div className="w-[100%] p-[10px]">
+                  <div className="px-[20px] flex mt-[5px] flex justify-start">
+                    <span className="flex justify-center items-center">
+                      <Star className="h-[15px] w-[15px] mr-[5px] flex justify-center items-center text-[yellow]" />
+                    </span>
+                    <span className="text-[14px]">
+                      {show.vote_average}
+                      <span className="text-[12px]">/10</span>
+                    </span>
+                  </div>
+                  <h2 className="text-sm mt-2 text-center">{show.name}</h2>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
       <button
         onClick={handleNavigate}
         className="bg-green-500 text-white px-4 py-2 rounded-md mt-4"
