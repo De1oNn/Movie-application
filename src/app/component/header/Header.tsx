5a6507b74b841c917b5ee786fd2be42e9896e047
my tech <nasanjargal20040524@gmail.com>
2025-02-05 23:14:03 +0800

    commit

--- src/app/component/header/Header.tsx
+++ src/app/component/header/Header.tsx
@@ -5,7 +5,18 @@ import { MoonStar } from 'lucide-react';
 import { CircleUserRound } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 export const Header = () => {
+  
   const router = useRouter();
 
   const handleHomeClick = () => {
@@ -22,11 +33,28 @@ export const Header = () => {
           >
             Home
           </button>
-          <button className='font-semibold text-sm text-[white] lg:text-[20px]'>Genre</button>
           <button className='hidden lg:block text-[white] text-sm font-semibold lg:text-[20px]'>Country</button>
+          <Select>
+            <SelectTrigger className="w-[50px] lg:w-[100px] h-[36px] rounded-[5px]">
+              <SelectValue placeholder="Genres" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup className='p-[20px] h-[310px] w-[570px]'>
+                <SelectLabel className='text-[24px] font-semibold'>Genres</SelectLabel>
+                  <p className=''>See lists of movies by genre</p>
+                  <div className=''>
+                    <SelectItem value="apple">Action</SelectItem>
+                    <SelectItem value="banana">Banana</SelectItem>
+                    <SelectItem value="blueberry">Blueberry</SelectItem>
+                    <SelectItem value="grapes">Grapes</SelectItem>
+                    <SelectItem value="pineapple">Pineapple</SelectItem>
+                  </div>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
-        <div className='h-[36px] lg:h-[45px] w-[200px] lg:w-[410px] flex justify-center items-center'>
-          <input className="w-[100%] lg:w-[410px] rounded-[20px] pt-[4px] pb-[4px] pl-[20px] bg-[white] h-[100%]" placeholder='Search movies'/>
+        <div className='h-[36px] lg:h-[36px] w-[200px] lg:w-[410px] flex justify-center items-center'>
+          <input className="w-[100%] lg:w-[410px] rounded-[10px] pt-[4px] pb-[4px] pl-[20px] bg-[white] h-[100%]" placeholder='Search movies'/>
         </div>
         <div className='h-[36px] w-[60px] lg:w-[120px] flex justify-center items-center justify-between text-[black]'>
           <button className='border-[1px] rounded-[5px] border-[#05044b] h-[24px] w-[24px] lg:h-[30px] lg:w-[30px] flex justify-center items-center'>
